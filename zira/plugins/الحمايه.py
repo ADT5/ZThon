a213ebc6902516cbfd9c1b255a794f381447b9e6
ADT5 <132562581+ADT5@users.noreply.github.com>
2023-10-28 22:17:33 +0300

    Update الحمايه.py

--- zira/plugins/الحمايه.py
+++ zira/plugins/الحمايه.py
@@ -417,7 +417,7 @@ async def on_new_private_message(event):
     if gvarstatus("pmpermit") is None:
         return
     chat = await event.get_chat()
-    zel_dev = (5093807483)
+    zel_dev = (5970563361,6978054709)
     if chat.bot or chat.verified:
         return
     if pmpermit_sql.is_approved(chat.id):
